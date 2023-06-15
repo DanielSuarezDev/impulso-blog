019c26b3693465928585a1bfee591a84bcbc412f
DanielSuarezDev <danielsuarez3114@gmail.com>
2023-06-15 12:34:21 -0500

    init project

---
+++ src/site-meta.config.ts
@@ -0,0 +1,12 @@
+const siteMeta = {
+	title: "impulsocristiano",
+	description:
+		"Impulsando tu ministerio con sana doctrina",
+	lang: "es-ES",
+	ogLocale: "es_ES",
+	githubUrl: "https://github.com/DanielSuarezDev/impulso-blog",
+	themeColorLight: "#fafafa",
+	themeColorDark: "#1d1f21",
+};
+
+export default siteMeta;
