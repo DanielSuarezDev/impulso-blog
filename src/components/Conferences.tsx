1f722a07a5ba48eae74658acdcc51c0ed44112ce
DanielSuarezDev <danielsuarez3114@gmail.com>
2023-06-20 13:26:04 -0500

    share icons

--- src/components/Conferences.tsx
+++ src/components/Conferences.tsx
@@ -1,18 +1,17 @@
 const Conferences = () => {
 	return (
 		<article>
-			<a href="#" className="relative top-0 block overflow-hidden rounded-2xl">
+			<a href="https://bbn1.bbnradio.org/spanish/" target="_blank" className="relative top-0 block overflow-hidden rounded-2xl">
 				<img
-					alt="Ultima conferencia impartida por Oscar Barajas @gndx"
-					src="https://arepa.s3.amazonaws.com/oscar-barajas-tavares-gndx-web3.png"
-					className="h-44 w-full object-cover"
+					alt="Emisora cristiana"
+					src="https://cdn-profiles.tunein.com/s130346/images/logog.png?t=1"
+					className="h-36 w-full object-cover"
 				/>
 				<div className="bg-gray-900 p-4">
-					<p className="mb-1 text-sm text-gray-400">Conferencias</p>
-					<h5 className="text-sm text-white">De la Antiguedad a Web3</h5>
+					<p className="mb-1 text-sm text-gray-400">Emisora</p>
+					<h5 className="text-sm text-white">Emisora totalmente cristiana</h5>
 					<p className="mt-1 text-xs text-gray-400">
-						Desde el antiguo Egipto, romanos, hasta la actualidad, repasamos las
-						bases que construyeron la criptografía...
+						Escucha o estudia en una emisora totalmente cristiana y de sana doctrina.
 					</p>
 				</div>
 			</a>
